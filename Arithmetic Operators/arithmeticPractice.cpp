5a9725903f4d2939adbbb562db8eb44bacc8625a
Zackery Loy <zackery.c.loy@gmail.com>
2026-03-16 02:06:53 -0400

    committing new topics

---
+++ Arithmetic Operators/arithmeticPractice.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+
+    // arithmetic operators - return the result of a specific
+    //                        arithmetic operation (+ - * /)
+
+
+    // precedence - parenthesis, multiplacation and division, addition and subtraction
+
+    int students = 20;
+    
+    // students = students + 1;
+    // students+=1;
+    // students++;
+
+    // students = students - 1;
+    // students=-1;
+    // students--;
+
+    // students = students * 2;
+    // students*=2;
+
+    // students = students / 2;
+    //  students/=2;
+
+
+    // int remainder = students % 2;
+    
+    
+
+
+    return 0;
+}
